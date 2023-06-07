00bd96b1d7da0b7e119286b19a8f14ad404dfc1d
k-seungri <sr930315@naver.com>
2023-06-07 17:46:03 +0900

    20230607

--- whisper_test.py
+++ whisper_test.py
@@ -1,5 +1,16 @@
 import whisper
+import os
+import time
 
-model = whisper.load_model("base")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+model = whisper.load_model("large")
+# # audio = whisper.load_audio("test_wav.wav")
+# result = model.transcribe("test_wav.wav",language='korean')
+# print(result["text"])
+
+files = os.listdir('./data/NO')
+for file in files:
+    s = time.time()
+    f = './data/NO/' + file
+    result = model.transcribe(f, language='korean')
+    e = time.time()
+    print(result['text'], e-s)
